e3c04e4999d34cfe11f125579f335c06c22e7f01
berloc <berloc1987@gmail.com>
2016-12-05 20:45:14 +0100

    updated main test file

--- src/frompythontojava/exercise1/Main.java
+++ src/frompythontojava/exercise1/Main.java
@@ -1,32 +1,45 @@
 package frompythontojava.exercise1;
 
+import frompythontojava.exercise1.models.FeatureProductCategory;
 import frompythontojava.exercise1.models.Product;
 import frompythontojava.exercise1.models.ProductCategory;
 import frompythontojava.exercise1.models.Supplier;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-        ProductCategory smartphone = new ProductCategory("phones", "phones");
+        ProductCategory smartPhone = new ProductCategory("phones", "phones");
         ProductCategory computer = new ProductCategory();
+        ProductCategory.addToProductCategryList(computer);
 
         Supplier apple = new Supplier();
         Supplier.addToSupplierList(apple);
-
         Supplier samsung = new Supplier("Samsung", "something");
 
 
-        Product phone1 = new Product("Iphone SE", 2342, smartphone, apple);
+        Product phone1 = new Product("Iphone SE", 2342, smartPhone, apple);
         Product macBook = new Product();
-        Product phone2 = new Product("Samsung 8s", 3342, smartphone, samsung);
+        Product.addToProductList(macBook);
+        Product phone2 = new Product("Samsung 8s", 3342, smartPhone, samsung);
+        Product MacBooker = new Product("MacBooker", 3333, computer, apple);
+
+        FeatureProductCategory someThing1 = new FeatureProductCategory();
+        FeatureProductCategory.addToProductCategryList(someThing1);
+        FeatureProductCategory someThing2 = new FeatureProductCategory("Something", "Description");
+
+        Date startDate = new Date();
+        Date endDate = new Date();
+        System.out.println(someThing2.schedule(startDate, endDate));
+
 
 
         System.out.println(phone1.toString());
         System.out.println(phone2.toString());
+        System.out.println(MacBooker.toString());
 
-//        System.out.println(phone1.getName());
-        System.out.println(Product.getProductList().size());
-        Product.addToProductList(macBook);
+
         System.out.println(Product.getProductList().size());
 
 
